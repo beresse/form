79af095d99e11af80dd5b7a63b61d63275a29589
Edmond <Edmondwilmore1er@gmail.com>
2021-02-24 01:16:48 +0100

    Dave

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Pressable,
+  Dimensions,
+  Alert,
+} from "react-native";
 
 export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setMdp] = useState("");
+
+  const submit = () => {
+    if (password.length <= 6) {
+      alert(`Je regrette, Dave. Cela m'est malheureusement impossible.`);
+    } else {
+      alert(
+        `Cette mission est trop importante pour moi. Je ne puis tolérer que vous la mettiez en péril.`
+      );
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Bonjour Dave</Text>
       <StatusBar style="auto" />
+      <View style={styles.input}>
+        <TextInput
+          style={styles.placeholderA}
+          placeholder="Email"
+          onChangeText={(email) => setEmail(email)}
+          value={email}
+          keyboardType={"email-address"}
+        />
+        <TextInput
+          style={styles.placeholderA}
+          secureTextEntry={true}
+          onChangeText={(password) => setMdp(password)}
+          value={password}
+          placeholder="Password"
+        />
+      </View>
+      <Pressable onPress={submit}>
+        <Text style={{ textAlign: "center" }}> Touch it </Text>
+      </Pressable>
     </View>
+
+/* <form onSubmit={this.handleSubmit}>
+<label>
+  Essay:
+  <textarea value={this.state.value} onChange={this.handleChange} />
+</label>
+<input type="submit" value="Envoyer" />
+</form> */
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  title:{
+    fontSize: 25,
+  },
+  input:{
+    marginVertical: 40,
+    alignContent: "center",
+  }
 });
